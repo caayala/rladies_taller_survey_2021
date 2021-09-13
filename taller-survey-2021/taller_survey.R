7518d2f46adc7db5c281b38bf2d4a93c1b34e844
Cristián <caayala@gmail.com>
2021-09-12 21:30:32 -0300

    Update taller_survey.R
    Ajustes de estilo

--- taller-survey-2021/taller_survey.R
+++ taller-survey-2021/taller_survey.R
@@ -27,9 +27,9 @@ summary(cas)
 ## La ventaja de srvyr es que puedes crear nuevas variables sin necesidad de volver a crear el objeto survey. 
 
 cas <- cas %>% mutate(catedad = case_when((edad <18) ~ 1,
-                                         (edad <30) ~ 2,
-                                         (edad <45) ~ 3,
-                                         (edad <60) ~ 4,
+                                          (edad <30) ~ 2,
+                                          (edad <45) ~ 3,
+                                          (edad <60) ~ 4,
                                           TRUE ~ 5),
                       fcatedad=factor(catedad),
                       cateduc=case_when((educ <5) ~ 1,
@@ -64,7 +64,7 @@ summary(cas_svy)
 ## SURVEY
 # Promedio de edad de los residentes de la VIII
 
-svymean( ~ edad, cas_svy, na.rm = TRUE) # error estándar
+svymean( ~ edad, cas_svy, na.rm = TRUE, deff = TRUE) # error estándar
 confint(svymean( ~ edad, cas_svy, na.rm = TRUE)) # intervalo de confianza
 cv(svymean( ~ edad, cas_svy, na.rm = TRUE)) # coeficiente de variación
 
@@ -83,20 +83,20 @@ svyquantile(~esc, cas_svy, na = TRUE, c(.25,.5,.75),ci=TRUE)
 
 svyhist(~edad, cas_svy)
 
-svyboxplot(~edad~1, cas_svy, all.outliers=TRUE)
+svyboxplot(edad ~ 1, cas_svy, all.outliers=TRUE)
 
-svyboxplot(~edad~fcateduc, cas_svy, all.outliers=TRUE)
+svyboxplot(edad ~ fcateduc, cas_svy, all.outliers=TRUE)
 
-svyboxplot(~edad~fpob, cas_svy, all.outliers=TRUE)
+svyboxplot(edad ~ fpob, cas_svy, all.outliers=TRUE)
 
 
 # Promedio de años de esc según catedgorías de edad de los residentes de VIII
 ## survey 
 svyby( ~ edad, ~ fcateduc, cas_svy, svymean, na.rm = TRUE)
-svyby( ~ esc, ~ fcatedad, cas_svy, svymean, na.rm = TRUE)
-## subset 
+svyby(  ~ esc, ~ fcatedad, cas_svy, svymean, na.rm = TRUE)
 
-cas24_svy <- subset(cas_svy, edad>24 & asiste==2)
+## subset 
+cas24_svy <- subset(cas_svy, edad > 24 & asiste == 2)
 svyby(  ~ esc, ~ fcatedad, cas24_svy, svymean, na.rm = TRUE)
 confint(svyby(  ~ esc, ~ fcatedad, cas24_svy, svymean, na.rm = TRUE))
 
@@ -111,22 +111,22 @@ svyttest(esc~sexo, cas24_svy)
 
 # test de medias: varias categorías
 # ¿Es diferente el promedio de educación del grupo 30-45 de aquellos con 60 años o más?
-
 svyby(~ esc, ~ catedad, cas24_svy,  na.rm = TRUE, svymean, covmat=TRUE)
 
 # "se porta mal" con los missing
-sv2 <- subset(cas24_svy, is.na(esc)==F)
+sv2 <- subset(cas24_svy, is.na(esc) == FALSE)
 
 svyby(~ esc, ~ catedad, sv2,  na.rm = TRUE, svymean, covmat=TRUE)
 
-t1 <- svycontrast(svyby(~ esc, ~ catedad, sv2,   na.rm = TRUE, svymean, covmat = TRUE), c( 0, -1,0,1))
+t1 <- svycontrast(svyby(~ esc, ~ catedad, sv2, na.rm = TRUE, svymean, covmat = TRUE), 
+                  c( 0, -1, 0, 1))
 
 t1
 confint(t1)
 
-## otra alternativa que maneja mejor los missing
-summary(svyglm(esc~fcatedad, design=cas24_svy))
-summary(svyglm(esc~relevel(fcatedad, ref = "3"), design=cas24_svy))
+## otra alternativa que maneja mejor los missing: usar regresión lineal.
+summary(svyglm(esc ~ fcatedad, design=cas24_svy))
+summary(svyglm(esc ~ relevel(fcatedad, ref = "3"), design=cas24_svy))
 
 
 
@@ -148,30 +148,30 @@ table(cep$estallido_6_a)
 # preparar base
 
 cep <- cep %>% mutate(
-                catedad = case_when((edad <30) ~ 2,
-                                         (edad <45) ~ 3,
-                                         (edad <60) ~ 4,
-                                          TRUE ~ 5),
-                      fcatedad=factor(catedad),
-                      cateduc=case_when((esc <5) ~ 1,
-                                        (esc <7) ~ 2,
-                                        (esc <20) ~ 3,
-                                         TRUE ~ 9),
-                      esc=ifelse(esc<90, esc, NA),
-                      fcateduc=factor(cateduc),
-                      apoyo=case_when((estallido_5<3)~1,
-                                      (estallido_5==3)~2,
-                                      (estallido_5<6)~3,
-                                      TRUE~8),
-                      manif=ifelse(estallido_6_a<6, estallido_6_a, 8),
-                      cerc_manif=case_when((estallido_7<3)~1,
-                                           (estallido_7==3)~2,
-                                           (estallido_7<6)~3,
-                                           TRUE ~ 8),
-                        fapoyo=factor(apoyo),
-                        fmanif=factor(manif),
-                        fcerc_manif=factor(cerc_manif))
-                
+  catedad = case_when((edad < 30) ~ 2,
+                      (edad < 45) ~ 3,
+                      (edad < 60) ~ 4,
+                      TRUE ~ 5),
+  fcatedad=factor(catedad),
+  cateduc=case_when((esc < 5) ~ 1,
+                    (esc < 7) ~ 2,
+                    (esc < 20) ~ 3,
+                    TRUE ~ 9),
+  esc=ifelse(esc<90, esc, NA),
+  fcateduc=factor(cateduc),
+  apoyo=case_when((estallido_5 < 3)~1,
+                  (estallido_5 == 3)~2,
+                  (estallido_5 < 6)~3,
+                  TRUE~8),
+  manif=ifelse(estallido_6_a<6, estallido_6_a, 8),
+  cerc_manif=case_when((estallido_7 < 3)~1,
+                       (estallido_7 == 3)~2,
+                       (estallido_7 < 6)~3,
+                       TRUE ~ 8),
+  fapoyo=factor(apoyo),
+  fmanif=factor(manif),
+  fcerc_manif=factor(cerc_manif))
+
 
 # Definir objeto survey según las variables de diseño
 
@@ -184,18 +184,21 @@ cep_svy = svydesign(
 
 ## Proporciones
 
-svymean( ~ fapoyo, cep_svy, na.rm = TRUE)
+svymean( ~ fapoyo, cep_svy, na.rm = TRUE) 
+# Con este método no se puede calcular el intervalo de confianza, 
+# No se debiese usar esta manera:
+# confint(svymean( ~ fapoyo, cep_svy, na.rm = TRUE))
 
-#method = c("logit", "likelihood", "asin", "beta", "mean","xlogit")
+# method = c("logit", "likelihood", "asin", "beta", "mean","xlogit")
 # proporciones 
-svyciprop( ~I(apoyo==1), cep_svy)
+svyciprop( ~I(apoyo==1), cep_svy, method = 'logit') # método por defecto
 svyciprop( ~I(apoyo==2), cep_svy)
 svyciprop( ~I(apoyo==3), cep_svy)
 svyciprop( ~I(apoyo==8), cep_svy)
 
 ### participó en manifestaciones
 ### 
-svymean( ~ fmanif, cep_svy, na.rm = TRUE)
+svymean( ~ fmanif, cep_svy, na.rm = TRUE, deff = TRUE)
 svyciprop( ~I(manif==1), cep_svy)
 svyciprop( ~I(manif==2), cep_svy)
 svyciprop( ~I(manif==3), cep_svy)
@@ -209,7 +212,7 @@ svyciprop( ~I(cerc_manif==3), cep_svy)
 svyciprop( ~I(cerc_manif==8), cep_svy)
 
 ## proporción por grupo
-aa <-svyby (  ~fmanif, ~ fcerc_manif, cep_svy, na.rm = T, svymean)
+aa <-svyby(~fmanif, ~ fcerc_manif, cep_svy, na.rm = T, svymean)
 
 round(aa[,2:5], 2)
 
@@ -224,7 +227,7 @@ svymean( ~ fmanif, cepsub_svy, na.rm = T)
 
 # ¿Es diferente la proporción de quienes participan en las manifestaciones según si cercanos los hicieron?
 
-barplt<-svyby(~fmanif, ~fcerc_manif, cep_svy, na = TRUE, svymean)
+barplt <- svyby(~fmanif, ~fcerc_manif, cep_svy, na = TRUE, svymean)
 barplot(barplt,beside=TRUE,legend=TRUE)
 
 svychisq(~fmanif+fcerc_manif, cep_svy, statistic="adjWald")
@@ -234,4 +237,5 @@ svychisq(~fmanif+fcerc_manif, cep_svy, statistic="adjWald")
 cepresp_svy <- subset(cep_svy, manif<6 & cerc_manif<6)
 
 round(svyby (  ~fmanif, ~ fcerc_manif, cepresp_svy, na.rm = T, svymean)[,2:4],2)
+
 svychisq(~fmanif+fcerc_manif, cepresp_svy, statistic="adjWald")
